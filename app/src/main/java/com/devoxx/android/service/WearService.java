a23693d34424ae0ed1ed36de5cfc9ba59c50f7b1
Said Eloudrhiri <info@noratek.net>
2016-03-19 16:20:55 +0100

    First implementation of the Wear app

---
+++ app/src/main/java/com/devoxx/android/service/WearService.java
@@ -0,0 +1,57 @@
+package com.devoxx.android.service;
+
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+import java.util.concurrent.TimeUnit;
+
+public class WearService extends WearableListenerService {
+
+    private final static String TAG = WearService.class.getCanonicalName();
+
+
+    protected GoogleApiClient mApiClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        mApiClient.connect();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mApiClient.disconnect();
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        super.onMessageReceived(messageEvent);
+
+        Log.d(TAG, "onMessageReceived");
+
+        //Ouvre une connexion vers la montre
+        ConnectionResult connectionResult = mApiClient.blockingConnect(30, TimeUnit.SECONDS);
+
+        if (!connectionResult.isSuccess()) {
+            Log.e(TAG, "Failed to connect to GoogleApiClient.");
+            return;
+        }
+
+        //traite le message reçu
+        final String path = messageEvent.getPath();
+
+        if(path.equals("bonjour")) {
+            int random = (int)(Math.random() * 100);
+            Log.d(TAG, "random number:" + random);
+        }
+    }
+}
